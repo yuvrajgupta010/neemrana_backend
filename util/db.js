39bf760ef6239be4069a678461f9b789a8cf2e6b
yuvraj gupta <yuvrajgupta010@gmail.com>
2023-06-17 23:04:04 +0530

    My hard work

--- util/db.js
+++ util/db.js
@@ -7,8 +7,9 @@ const pgp = require("pg-promise")({
 
 // Preparing the connection details:
 // const cn = "postgres://tcs_admin:root@localhost:5432/tcs_ion";
-
-const cn = `postgres://${process.env.username}:${process.env.password}@dpg-ci6okh18g3nfucc60460-a.singapore-postgres.render.com/hotel_booking_jlj4`;
+// const cn = `postgres://${process.env.username}:${process.env.password}@dpg-ci6okh18g3nfucc60460-a.singapore-postgres.render.com/hotel_booking_jlj4`;
+// const cn = `postgres://yuvraj:cY9lu2pEceLj1r0U1odwgXDeEVVVD3Ng@dpg-ci6okh18g3nfucc60460-a.singapore-postgres.render.com/hotel_booking_jlj4?ssl=true`;
+const cn = `postgres://yuvraj:cY9lu2pEceLj1r0U1odwgXDeEVVVD3Ng@dpg-ci6okh18g3nfucc60460-a/hotel_booking_jlj4?ssl=true`;
 
 // Creating a new database instance from the connection details:
 const db = pgp(cn);
