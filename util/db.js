a4a22188477ee436945832571a0a90f1e480e27f
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2023-10-01 08:32:46 +0530

    Update db.js

--- util/db.js
+++ util/db.js
@@ -7,7 +7,7 @@ const pgp = require("pg-promise")({
 
 // Preparing the connection details:
 // const cn = "postgres://tcs_admin:root@localhost:5432/tcs_ion";
-const cn = `postgres://${process.env.username}:${process.env.password}@dpg-ci6okh18g3nfucc60460-a.singapore-postgres.render.com/hotel_booking_jlj4?ssl=true`;
+const cn = `postgres://${process.env.username}:${process.env.password}@dpg-ckcduvesmu8c73c6fj2g-a.singapore-postgres.render.com/hotel_booking_qh7d?ssl=true`;
 
 // Creating a new database instance from the connection details:
 const db = pgp(cn);
